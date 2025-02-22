de10d45c14dc175196d2d7d2f16d935a20390100
pr0fix <bgx152@myy.haaga-helia.fi>
2025-02-22 14:50:21 +0200

    update tsconfig and move index to src-folder

--- index.ts
+++
@@ -1,7 +0,0 @@
-export { brewLength } from "./src/brewLength";
-export { compareBeans } from "./src/compareBeans";
-export { findFirstSip } from "./src/findFirstSip";
-export { findFlavor } from "./src/findFlavor";
-export { froth } from "./src/froth";
-export { pourCoffee } from "./src/pourCoffee";
-export { sipAt } from "./src/sipAt";
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { brewLength } from "./brewLength";
+export { compareBeans } from "./compareBeans";
+export { findFirstSip } from "./findFirstSip";
+export { findFlavor } from "./findFlavor";
+export { froth } from "./froth";
+export { pourCoffee } from "./pourCoffee";
+export { sipAt } from "./sipAt";
